8c0df4969bc4607f6835a4f3c721417de19ea7df
ricardomoura <ricardo.souza.moura@gmail.com>
2018-09-14 06:16:51 -0300

    adapter para o grid de temporada

--- feature/src/main/java/com/zimba/f1/feature/SeasonFragment.java
+++ feature/src/main/java/com/zimba/f1/feature/SeasonFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.zimba.f1.feature.entity.SeasonGridEntity;
 
 
 /**
@@ -23,7 +26,28 @@ public class SeasonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_season, container, false);
+        View seasonFragment = inflater.inflate(R.layout.fragment_season, container, false);
+
+        GridView gridview = (GridView) seasonFragment.findViewById(R.id.gridSeason);
+
+        SeasonGridEntity[] arraySeason = new SeasonGridEntity[2];
+
+        SeasonGridEntity season1 = new SeasonGridEntity();
+        season1.setConstructorChampionName("Ferrari");
+        season1.setDriverChampionName("Barrichelo");
+        season1.setSeasonYear("2018");
+        arraySeason[0] = season1;
+
+        SeasonGridEntity season2 = new SeasonGridEntity();
+        season2.setConstructorChampionName("Mercedes");
+        season2.setDriverChampionName("Hamilton");
+        season2.setSeasonYear("2018");
+        arraySeason[1] = season2;
+
+        gridview.setAdapter(new SeasonAdapter(this.getContext(), arraySeason));
+
+        return seasonFragment;
+
     }
 
 }
