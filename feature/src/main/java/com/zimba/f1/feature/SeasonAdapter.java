2426d1388a21a070dd772802f63e07bda4279333
ricardomoura <ricardo.souza.moura@gmail.com>
2018-09-14 06:19:05 -0300

    adapter para o grid de temporada

---
+++ feature/src/main/java/com/zimba/f1/feature/SeasonAdapter.java
@@ -0,0 +1,63 @@
+package com.zimba.f1.feature;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zimba.f1.feature.entity.SeasonGridEntity;
+
+public class SeasonAdapter extends BaseAdapter {
+
+    private Context context;
+    private SeasonGridEntity[] seasonGridEntities;
+
+    public SeasonAdapter(Context pContext, SeasonGridEntity[] pSeasonGridEntities){
+        this.context = pContext;
+        this.seasonGridEntities = pSeasonGridEntities;
+    }
+    @Override
+    public int getCount() {
+        return seasonGridEntities.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return seasonGridEntities[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LinearLayout seasonGridLayout;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            seasonGridLayout = new LinearLayout(context);
+            seasonGridLayout.setOrientation(LinearLayout.VERTICAL);
+
+            SeasonGridEntity seasonGridEntity = seasonGridEntities[position];
+
+            TextView driverChampionNameTV = new TextView(context);
+            driverChampionNameTV.setText(seasonGridEntity.getDriverChampionName());
+            seasonGridLayout.addView(driverChampionNameTV);
+
+            TextView seasonYearTV = new TextView(context);
+            seasonYearTV.setText(seasonGridEntity.getSeasonYear());
+            seasonGridLayout.addView(seasonYearTV);
+
+            TextView constructorChampionNameTV = new TextView(context);
+            constructorChampionNameTV.setText(seasonGridEntity.getConstructorChampionName());
+            seasonGridLayout.addView(constructorChampionNameTV);
+
+        } else {
+            seasonGridLayout = (LinearLayout) convertView;
+        }
+        return seasonGridLayout;
+    }
+}
---
+++ feature/src/main/java/com/zimba/f1/feature/entity/SeasonGridEntity.java
@@ -0,0 +1,33 @@
+package com.zimba.f1.feature.entity;
+
+public class SeasonGridEntity {
+
+    private String driverChampionName;
+    private String seasonYear;
+    private String constructorChampionName;
+
+    public String getDriverChampionName() {
+        return driverChampionName;
+    }
+
+    public void setDriverChampionName(String driverChampionName) {
+        this.driverChampionName = driverChampionName;
+    }
+
+    public String getSeasonYear() {
+        return seasonYear;
+    }
+
+    public void setSeasonYear(String seasonYear) {
+        this.seasonYear = seasonYear;
+    }
+
+    public String getConstructorChampionName() {
+        return constructorChampionName;
+    }
+
+    public void setConstructorChampionName(String constructorChampionName) {
+        this.constructorChampionName = constructorChampionName;
+    }
+
+}
