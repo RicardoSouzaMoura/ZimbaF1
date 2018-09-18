931f5548747098e6b2989d24c8068d965ddb08cd
RICARDO DE SOUZA MOURA <ricardo.moura@petrobras.com.br>
2018-09-18 16:58:16 -0300

    trabalho1

--- feature/src/main/java/com/zimba/f1/feature/entity/SeasonGridEntity.java
+++ feature/src/main/java/com/zimba/f1/feature/entity/SeasonGridEntity.java
@@ -30,4 +30,12 @@ public class SeasonGridEntity {
         this.constructorChampionName = constructorChampionName;
     }
 
+    public SeasonGridEntity[] findAllSeasonGridEntities(){
+        SeasonGridEntity[] entities;
+
+
+
+        return entities;
+    }
+
 }
